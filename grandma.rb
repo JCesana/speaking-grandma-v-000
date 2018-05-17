d268dd9d8a6f35ef6c9858d02c8c950d1d6bbdff
Josh Cesana <joshuacesana@gmail.com>
2018-05-17 18:42:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase = "I LOVE YOU GRANDMA!"
+  if phrase != phrase.upcase
+    ""
+  elseif phrase = "I LOVEYOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   end 
 end
